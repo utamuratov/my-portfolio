3380152912c661979d67bcedfe1e61b8aafe2fb0
utamuratov <43562558+utamuratov@users.noreply.github.com>
2025-10-15 14:50:37 +0500

    projects: uzbekistan-map added

--- src/app/home/projects/projects.component.ts
+++ src/app/home/projects/projects.component.ts
@@ -8,6 +8,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     <div id="projects" class="container">
       <h2 class="linier-title text-center pt-6">Projects</h2>
       <div class="cards mt-2">
+        <a class="card" href="https://map.utamuratov.uz/" target="_blank">
+          <div class="card__body">
+            <img src="./assets/images/ngx-uzbekistan-map.png" />
+          </div>
+          <div class="card__footer">
+            <span>CLICK HERE TO VISIT</span>
+            <h3>Uzbekistan map</h3>
+          </div>
+        </a>
         <a class="card" href="https://t.me/hbirthday2you_bot" target="_blank">
           <div class="card__body">
             <img src="./assets/images/hbirthday.jpg" />

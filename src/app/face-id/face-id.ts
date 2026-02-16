4535e41b247a5f9ba9b650529f58483ba929c4c2
utamuratov <43562558+utamuratov@users.noreply.github.com>
2026-02-16 10:11:21 +0500

    face-id neew version

--- src/app/face-id/face-id.ts
+++ src/app/face-id/face-id.ts
@@ -1,16 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { NgxFaceLiveness } from '@utamuratov/ngx-face-id';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FaceIdService, NgxFaceLiveness } from '@utamuratov/ngx-face-id';
 
 @Component({
   selector: 'app-face-id',
   standalone: true,
   imports: [NgxFaceLiveness],
   template: `
-    <div style="min-height: 500px;">
+    <div style="min-height: 500px; padding: 40px 0">
       <ngx-face-liveness />
     </div>
   `,
   styleUrl: './face-id.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FaceId {}
+export class FaceId {
+  private faceId = inject(FaceIdService);
+  constructor() {
+    this.faceId.loadModels();
+  }
+}

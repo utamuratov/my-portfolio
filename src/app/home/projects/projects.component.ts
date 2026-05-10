b946d18aa48ed721094b7873f8222672736f3e12
utamuratov <43562558+utamuratov@users.noreply.github.com>
2026-05-10 16:37:20 +0500

    Refactor code structure for improved readability and maintainability

--- src/app/home/projects/projects.component.ts
+++ src/app/home/projects/projects.component.ts
@@ -1,12 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-    selector: 'app-projects',
-    imports: [],
-    template: `
+  selector: 'app-projects',
+  imports: [],
+  template: `
     <div id="projects" class="container">
       <h2 class="linier-title text-center pt-6">Projects</h2>
       <div class="cards mt-2">
+        <a class="card" href="https://youtu.be/KVrJOHlcPe4" target="_blank">
+          <div class="card__body">
+            <img src="./assets/images/formlingo-promo.jpg" alt="" />
+          </div>
+          <div class="card__footer">
+            <span>CLICK HERE TO VISIT</span>
+            <h3>FormLingo</h3>
+          </div>
+        </a>
         <a class="card" href="https://map.utamuratov.uz/" target="_blank">
           <div class="card__body">
             <img src="./assets/images/ngx-uzbekistan-map.png" />
@@ -38,19 +47,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
             <h3>Cyrillic-Latin</h3>
           </div>
         </a>
-        <a class="card" href="https://uzimizniki.agrozamin.uz/" target="_blank">
-          <div class="card__body">
-            <img src="./assets/images/uzimizniki.jpg" alt="" />
-          </div>
-          <div class="card__footer">
-            <span>CLICK HERE TO VISIT</span>
-            <h3>O'zimizniki</h3>
-          </div>
-        </a>
       </div>
     </div>
   `,
-    styleUrl: './projects.component.less',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrl: './projects.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectsComponent {}
